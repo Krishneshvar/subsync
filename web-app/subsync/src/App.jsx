9caf18017b2f11b6a878039b39885558b1590a00
Krishneshvar <krishjn07@gmail.com>
2024-10-30 09:16:35 +0530

    Login and routing errors rectified.

--- web-app/subsync/src/App.jsx
+++ web-app/subsync/src/App.jsx
@@ -1,16 +1,24 @@
-import './App.css'
-import Login from './components/AuthForm/Login'
-import Dashboard from './components/Dashboard/Dashboard'
+import './App.css';
+import Login from './components/AuthForm/Login';
+import Dashboard from './components/Dashboard/Dashboard';
+import Customers from './components/Management/Customers/Customers';
+import Products from './components/Management/Products/Products';
+import Subscriptions from './components/Management/Subscriptions/Subscriptions';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
-  return(
-    <>
-      {/* <div className='login-form'>
-        <Login />
-      </div> */}
-      <Dashboard />
-    </>
-  )
+
+  const router = createBrowserRouter([
+    { path: "/", element: <Login /> },
+    { path: "/dashboard", element: <Dashboard /> },
+    { path: "/customers", element: <Customers />, },
+    { path: "/products", element: <Products />, },
+    { path: "/subscriptions", element: <Subscriptions />, },
+  ]);
+
+  return (
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
--- web-app/subsync/src/components/Dashboard/Dashboard.jsx
+++ web-app/subsync/src/components/Dashboard/Dashboard.jsx
@@ -1,36 +1,20 @@
-import './Dashboard.css'
-import Sidebar from '../Sidebar/Sidebar'
-import Navbar from '../Navbar/Navbar'
-import Home from '../Home/Home'
-import Customers from '../Management/Customers/customers'
-import Products from '../Management/Products/products'
-import Subscriptions from '../Management/Subscriptions/subscriptions'
-import { createBrowserRouter, Outlet, RouterProvider, } from "react-router-dom";
-
-const router = createBrowserRouter([
-  { path: "/", element: <Home />, },
-  { path: "/customers", element: <Customers />, },
-  { path: "/products", element: <Products />, },
-  { path: "/subscriptions", element: <Subscriptions />, },
-]);
+import './Dashboard.css';
+import Sidebar from '../Sidebar/Sidebar';
+import Navbar from '../Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
 
 function Dashboard() {
-
   return (
-    <>
-      <div className='app-container'>
-        <div className='main'>
-          <Sidebar />
-          <div className='contents'>
-            <Navbar />
-            <RouterProvider router={router} />
-            <Outlet />
-          </div>
+    <div className='app-container'>
+      <div className='main'>
+        <Sidebar />
+        <div className='contents'>
+          <Navbar />
+          <Outlet />
         </div>
-        {/* <Footer /> */}
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
