bb473ad4acc9ced870920708666b4bbdb29572c4
Krishneshvar <krishjn07@gmail.com>
2025-06-09 13:08:51 +0530

    chore: Remove backend redundancy

--- web-app/server/db/migrations/005_update_services_table.sql
+++
@@ -1,8 +0,0 @@
--- Update services table to use vendor_id as a foreign key
-ALTER TABLE services
-    MODIFY COLUMN preferred_vendor VARCHAR(20),
-    ADD CONSTRAINT fk_preferred_vendor
-    FOREIGN KEY (preferred_vendor)
-    REFERENCES vendors(vendor_id)
-    ON DELETE SET NULL
-    ON UPDATE CASCADE; 
--- web-app/server/db/migrations/update_taxes_table.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE taxes
-  ADD COLUMN IF NOT EXISTS tax_rates JSON DEFAULT '[]',
-  ADD COLUMN IF NOT EXISTS default_tax_preference JSON DEFAULT '{}',
-  ADD COLUMN IF NOT EXISTS gst_settings JSON DEFAULT '{}';
